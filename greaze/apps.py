0cae31a2f55390b32f0a21936107281469f00526
Cian <cian.omondi@student.moringaschool.com>
2021-09-16 21:39:09 +0300

    Configured app

--- greaze/apps.py
+++ greaze/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class GreazeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'greaze'
+
+    def ready(self):
+        import greaze.signals
